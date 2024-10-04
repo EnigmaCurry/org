0bf2c47dbc3c65553b9fc9f5e810b0dfee87621a
EnigmaCurry <ryan@enigmacurry.com>
2024-10-04 11:09:21 -0600

    add build log and gh-action cache

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,14 @@
 ;; This Emacs config is setup for batch mode only, for building ox-hugo sites.
 ;; It is not a general purpose emacs config!
 
-(message load-file-name)
+(message "Loading init.el from %s" load-file-name)
+
+(message "Setting user-emacs-directory")
 (setq user-emacs-directory "~/git/vendor/enigmacurry/org/emacs.d")
+
 (defvar bootstrap-version)
+
+(message "Starting bootstrap process")
 (let
   (
     (bootstrap-file
@@ -20,12 +25,16 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
+(message "Bootstrap process complete")
 
 ;; Use-package for all dependencies :: https://github.com/jwiegley/use-package
+(message "Installing use-package")
 (straight-use-package 'use-package)
 ;; Make use-package use straight.el by default:
 (setq straight-use-package-by-default t)
+(message "use-package installed and configured")
 
+(message "Configuring ox-hugo")
 (use-package ox-hugo
   :after org
   :config
@@ -39,21 +48,23 @@
           ("stdout" :raw t)
           ("edit" :raw t)
           ("env" :raw t)
-          ("math" :raw t)))
-  )
+          ("math" :raw t))))
 
 (use-package f)
 
 (defun build (root-dir)
   "Build all org books into hugo markdown"
+  (message "Starting build process for %s" root-dir)
   (let ((org-files (append
                     (directory-files root-dir "\\.org$")
                     (directory-files (f-join root-dir "books") t "\\.org$"))))
     (dolist (e org-files)
+      (message "Processing file: %s" e)
       (find-file e)
-      (org-hugo-export-wim-to-md :all-subtrees nil nil :noerror))))
-
+      (org-hugo-export-wim-to-md :all-subtrees nil nil :noerror)))
+  (message "Build process complete"))
 
 ;; Run this occasionally, to lock new package versions:
-;;(straight-freeze-versions)
+;; (straight-freeze-versions)
 
+(message "init.el loading complete")
