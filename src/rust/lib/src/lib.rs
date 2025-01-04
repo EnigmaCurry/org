ac9558befd0a670df4fdeefa939636cf9228e28c
EnigmaCurry <ryan@enigmacurry.com>
2025-01-03 18:05:32 -0700

    org-babel rust lib

---
+++ src/rust/lib/src/lib.rs
@@ -0,0 +1,26 @@
+pub use plotters;
+use std::env;
+use std::path::Path;
+
+pub fn get_static_file(filename: &str) -> Result<String, Box<dyn std::error::Error>> {
+    Ok(format!("{}/{}", env::var("OX_HUGO_STATIC")?, filename))
+}
+
+pub fn get_static_file_url(filename: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let org_dir = &env::var("ORG_DIR")?;
+    let org_dir = Path::new(&org_dir);
+    let full_path = Path::new(filename);
+    Ok(format!(
+        "file:../{}",
+        full_path
+            .strip_prefix(org_dir)?
+            .to_str()
+            .unwrap()
+            .trim_start_matches('/')
+    ))
+}
+
+pub fn get_static_file_link(filename: &str) -> Result<(), Box<dyn std::error::Error>> {
+    println!("[[{}]]", get_static_file_url(filename)?);
+    Ok(())
+}
